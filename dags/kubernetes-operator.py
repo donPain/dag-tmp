c2818668bf364b580ac0b6e8b35553d30a7a99f8
eduardo <eduardo.plis@hotmail.com>
2023-10-23 18:59:49 -0300

    test

--- dags/kubernetes-operator.py
+++ dags/kubernetes-operator.py
@@ -32,11 +32,11 @@ with DAG(
     osmosis_update_file_task = KubernetesPodOperator(
         name="osmosis-processor",
         cmds=["bash", "-cx"],
-        arguments=["/osmosis/package/bin/osmosis --help",  "echo '{\"success\": true}' >> /airflow/xcom/return.json"],
+        arguments=["/osmosis/package/bin/osmosis --help >> /dev/null 2>&1",  "echo '{\"success\": true}' >> /airflow/xcom/return.json"],
         image='334077612733.dkr.ecr.sa-east-1.amazonaws.com/routes/osmosis:latest',
         image_pull_secrets='aws-cred-new',
         startup_timeout_seconds=900,
-        # do_xcom_push=True,
+        do_xcom_push=True,
         task_id="osmosis"
     )
 

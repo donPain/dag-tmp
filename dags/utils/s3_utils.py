319b066ec9e0d1a27062f8fbbedebdb714af2aba
eduardo <eduardo.plis@hotmail.com>
2023-10-26 15:26:39 -0300

    check if key exists

--- dags/utils/s3_utils.py
+++ dags/utils/s3_utils.py
@@ -5,11 +5,14 @@ S3_HOOK = S3Hook(aws_conn_id="aws_default")
 
 def upload_file(local_path, key, bucket):
     try:
-        S3_HOOK.load_file(
-            filename=local_path,
-            key=key,
-            bucket_name=bucket
+        if not S3_HOOK.check_for_key(key, bucket):
+            S3_HOOK.load_file(
+                filename=local_path,
+                key=key,
+                bucket_name=bucket
             )
-        print(f"Arquivo {key} carregado com sucesso no Amazon S3")
+            print(f"Arquivo {key} carregado com sucesso no Amazon S3")
+        else:
+            print(f'O arquivo {key} já existe no S3. Não será substituído.')
     except AirflowException as e:
         print(f"Erro ao carregar o arquivo {key} para o Amazon S3: {str(e)}")
